f52bf2b7f47394c180953f98db518d3b579d21dd
Roman Kalachik <roman.kalachik@devexpress.com>
2020-07-28 17:55:42 +0300

    first commit

---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Diagnostics.Windows;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dispatcher dispatcher;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dispatcher = Dispatcher;
+            Loaded += MainWindow_Loaded;
+        }
+
+        public static Summary StartTest()
+        {
+            ManualConfig config = new ManualConfig()
+      .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+                .AddValidator(JitOptimizationsValidator.DontFailOnError)
+                .AddDiagnoser(new PmcDiagnoser())
+                .AddHardwareCounters(HardwareCounter.BranchInstructions)
+                .AddLogger(ConsoleLogger.Default)
+                .AddColumnProvider(DefaultColumnProviders.Instance);
+            return BenchmarkRunner.Run<Test>(config);
+        }
+        void MainWindow_Loaded(object sender, RoutedEventArgs e) {
+            StartTest();
+        }
+    }
+
+    [TestFixture]
+    public class TestAdapter {
+        [Test]
+        public void DoTest() {
+            var res = MainWindow.StartTest();
+            Assert.Less(int.Parse( res.Table.Columns[44].Content[0].Replace(",", "")), 115212800);
+        }
+    }
+}
---
+++ WpfApp14/Test.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Threading;
+namespace WpfApp14 {
+    [SimpleJob(RunStrategy.ColdStart, targetCount: 1)]
+    public class Test {
+        Window window;
+        void DoEvents() {
+            window.Dispatcher.Invoke(DispatcherPriority.Background,
+                                          new Action(delegate { }));
+        }
+        [Benchmark]
+        [STAThread]
+        public void Resize() {
+            window = new Window();
+            window.Show();
+            window.Width++;
+            DoEvents();
+            window.Width--;
+            DoEvents();
+            window.Close();
+        }
+    }
+}
